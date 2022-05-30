a54e39817a7c91afff2e8b48589aa49edb3a0255
Tejaswini Mahale <tejaswinimahale11@gmail.com>
2022-05-30 10:17:35 +0530

    latest todo app updated

--- week2/day1/day1w/src/App.js
+++ week2/day1/day1w/src/App.js
@@ -1,72 +1,66 @@
 
-// import react,{useEffect, useState} from "react"
-// function App() {
-//   const [todos,setTodos]=useState([])
-//   const [newTodo,setNewTodo]=useState("")
-//   useEffect(()=>{
-//     fetch("http://localhost:3000/teju")  
-//     .then((r)=>r.json())
-//     .then((d)=>{
-//       console.log(d)
-//       setTodos(d)
-//     })
-//   },[])
-//   const saveInfo=()=>{
-//     fetch("http://localhost:3000/teju",{
-//       method:"POST",
-//       headers:{
-//         "content-type":"application/json",
-//       },
-//       body:JSON.stringify({
-//         name:newTodo,
-//       isCompleted:false,
-//       }),
-    
-//     })
-//     .then((r)=>r.json())
-//     .then((d)=>{
-//       setTodos([...todos,d])
-//       newTodo("")
-//     })
-//   }
-//     return(
-//       <>
-//         <div>Todo</div>
-//         <div>
-//         <input value={newTodo}
-//           onChange={({target})=>setNewTodo(target.value)}
-//         />
-//         <button onClick={saveInfo}>+</button>
-//         </div>
-//         {todos.map((todo)=>(
-//           <div key={todo.id}>{todo.name}</div>
-//         ))}
-//       </>
-//     )
-// }
-
-// export default App;
-
-
-import React, { useEffect, useState } from 'react'
- import axios from "axios"
-const App = () => {
+import React,{useEffect, useState} from "react"
+import axios from "axios"
+ function App() {
+  const [page,setPage]=useState(1)
   const [todos,setTodos]=useState([])
+  const [newTodo,setNewTodo]=useState("")
+  // useEffect(()=>{
+  //   fetch("http://localhost:3000/teju")  
+  //   .then((r)=>r.json())
+  //   .then((d)=>{
+  //     console.log(d)
+  //     setTodos(d)
+  //   })
+  // },[])
   useEffect(()=>{
     
-    axios.get("http://localhost:3000/teju").then((r)=>{
+    axios.get(`http://localhost:3000/teju?_page=${page}&_limit=2`).then((r)=>{
     setTodos(r.data)
     console.log(r.data)
-  });
-  },[])   
-   
-  return (
-    <div>
+  })
+  },[page]) 
+  const saveInfo=()=>{
+    fetch("http://localhost:3000/teju",{
+      method:"POST",
+      headers:{
+        "content-type":"application/json",
+      },
+      body:JSON.stringify({
+        name:newTodo,
+      isCompleted:false,
+      }),
+    
+    })
+    .then((r)=>r.json())
+    .then((d)=>{
+      setTodos([...todos,d])
+      setNewTodo("")
+    })
+  }
+    return(
+      <>
+        <div>Todo</div>
+        <div>
+        <input value={newTodo}
+          onChange={({target})=>setNewTodo(target.value)}
+        />
+        <button onClick={saveInfo}>+</button>
+        </div>
+         <div>
         {
           todos.map((todo)=><div key={todo.id}>{todo.name}</div>)
         }
-    </div>
-  )
+        <button 
+          disabled={page<=1}
+          onClick={()=>{
+          if(page>1){
+            setPage(page-1)
+          }
+        }}>{`<`}</button>
+        <button onClick={()=>setPage(page+1)}>{`>`}</button>
+        </div>
+      </>
+    )
 }
-
 export default App
